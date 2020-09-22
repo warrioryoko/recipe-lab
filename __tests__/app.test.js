58ee3b1f0ae4b67bf1766d778878812e2bdf6b9a
Ron Yonker <miangpellegri@gmail.com>
2020-09-22 13:24:36 -0700

    Initial commit

---
+++ __tests__/app.test.js
@@ -0,0 +1,89 @@
+const fs = require('fs');
+const pool = require('../lib/utils/pool');
+const request = require('supertest');
+const app = require('../lib/app');
+const Recipe = require('../lib/models/recipe');
+
+describe('recipe-lab routes', () => {
+  beforeEach(() => {
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+  });
+
+  it('creates a recipe', () => {
+    return request(app)
+      .post('/api/v1/recipes')
+      .send({
+        name: 'cookies',
+        directions: [
+          'preheat oven to 375',
+          'mix ingredients',
+          'put dough on cookie sheet',
+          'bake for 10 minutes'
+        ]
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          id: expect.any(String),
+          name: 'cookies',
+          directions: [
+            'preheat oven to 375',
+            'mix ingredients',
+            'put dough on cookie sheet',
+            'bake for 10 minutes'
+          ]
+        });
+      });
+  });
+
+  it('gets all recipes', async() => {
+    const recipes = await Promise.all([
+      { name: 'cookies', directions: [] },
+      { name: 'cake', directions: [] },
+      { name: 'pie', directions: [] }
+    ].map(recipe => Recipe.insert(recipe)));
+
+    return request(app)
+      .get('/api/v1/recipes')
+      .then(res => {
+        recipes.forEach(recipe => {
+          expect(res.body).toContainEqual(recipe);
+        });
+      });
+  });
+
+  it('updates a recipe by id', async() => {
+    const recipe = await Recipe.insert({
+      name: 'cookies',
+      directions: [
+        'preheat oven to 375',
+        'mix ingredients',
+        'put dough on cookie sheet',
+        'bake for 10 minutes'
+      ],
+    });
+
+    return request(app)
+      .put(`/api/v1/recipes/${recipe.id}`)
+      .send({
+        name: 'good cookies',
+        directions: [
+          'preheat oven to 375',
+          'mix ingredients',
+          'put dough on cookie sheet',
+          'bake for 10 minutes'
+        ]
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          id: expect.any(String),
+          name: 'good cookies',
+          directions: [
+            'preheat oven to 375',
+            'mix ingredients',
+            'put dough on cookie sheet',
+            'bake for 10 minutes'
+          ]
+        });
+      });
+  });
+});
---
+++ sql/setup.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS recipes;
+
+CREATE TABLE recipes (
+  id BIGINT GENERATED ALWAYS AS IDENTITY,
+  name TEXT NOT NULL,
+  directions TEXT[]
+);
